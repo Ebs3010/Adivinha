308b56aa3a4d7f1075f15ea709bf917af58614f2
Ebs3010 <evertonbs3010@gmail.com>
2016-05-01 22:45:45 -0300

    Added files via upload

--- Server_Main.py
+++ Server_Main.py
@@ -1,17 +1,30 @@
 
 import socket
 import random
+from thread import *
+import sys
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = socket.gethostname()
-PORT = 6008
+PORT = 6031
 tcp.bind((HOST, PORT))
 
+print ("Socket criado com sucesso!!\n")
+
+#Numeros de conexoes permitidas
 tcp.listen(2)
 
+#
+def clientthread(con):
+        while True:
+            guess(con)
+        #con.send("Congratulations!!")
+
+        #con.close
+
 #As comparacoes sao feitas dentro desta funcao
-def guess ():
-    jog1_number = int (con.recv(1024))
+def guess (con):
+    jog1_number = int(con.recv(1024))
     
     if jog1_number == number:  
         #print("Congratulations!!")
@@ -25,7 +38,9 @@ def guess ():
 
         else:
             #print("Try a big number")
-            con.send("Try a big number")        
+            con.send("Try a big number")
+            
+    con.close    
       
 
 #O servidor gera um numero aleatorio entre 1 e 60
@@ -34,14 +49,22 @@ number = random.randint(1, 60)
 while True:
     con, client = tcp.accept()
     print("Good job!", client)
-    print ("Number", number)
+    print ("Number", number)       
     
     print "Try to guess the number between 1 and 60:"
-    while True:        
-        guess()
+    
+    start_new_thread(clientthread, (con,))
 
-    print ("Finished conection.", client)
-    con.close()  
+print ("Finished conection.", client)
+con.close()
+tcp.close()  
+        
+
+
+
+
+        
+        
         
 
 
